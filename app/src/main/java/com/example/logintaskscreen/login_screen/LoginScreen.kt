a528b82ab09ae015922450144b1425ce1b257dd4
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-07-02 18:58:09 +0300

    init LoginScreen

---
+++ app/src/main/java/com/example/logintaskscreen/login_screen/LoginScreen.kt
@@ -0,0 +1,30 @@
+package com.example.logintaskscreen.login_screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.logintaskscreen.R
+import com.example.logintaskscreen.composable.ApplicationLogo
+import com.example.logintaskscreen.ui.theme.BackgroundScreenColor
+
+@Composable
+fun LoginScreen(){
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewLoginScreen(){
+    LoginScreen()
+}
